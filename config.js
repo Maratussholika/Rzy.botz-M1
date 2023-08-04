56169336e0446986f57fdb3ed90270d06e150627
Maratussholika <140876227+Maratussholika@users.noreply.github.com>
2023-08-04 09:38:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285785694474', '6285785694474','6285828764046','6285828764046','6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6285204874976', '6285204874976','6285828764046','6285828764046','6285828764046'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -40,7 +40,7 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Rzy-bot'
+global.packname = ''
 global.author = 'Rzy-bot'
 
 global.wait = '「 ⏱️ 」Harap tunggu sebentar...'
